06ef37a50c0ecf0e7b23d18196c6fc09ec32d0d3
omiras <oscar.miras@gmail.com>
2023-04-05 10:17:23 +0000

    exercise ready

---
+++ src/js/component/WordCount.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function WordCount() {
+  return (
+    <div className="container d-flex flex-column gap-4">
+      <div className="form-group fs-4">
+        <label for="word">Type a word in the box
+:</label>
+        <input type="text" className="form-control fs-3 mt-2" id="word" />
+      </div>
+      <div className="fw-bold container bg-info p-2">Word Count: 0</div>
+      <button className="btn btn-success">Add word to the list</button>
+    </div>
+  );
+}
